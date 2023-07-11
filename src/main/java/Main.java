a0de98949493802111c415c8cb3a7d38575602f7
Oleg <mayakovskiyoleg@mail.ru>
2023-07-11 17:19:00 +0300

    add code

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 444444444";
+        post.patronimic = "Иванович";
+        post.phone = "+7(999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 4;
+        post.birthday.year = 1993;
+    }
+}
